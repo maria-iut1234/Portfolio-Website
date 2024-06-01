ffdac245be2b3048117da8ab395949973f1ab5a3
Shanta Maria <shantamaria@iut-dhaka.edu>
2024-06-02 03:38:21 +0600

    more for deploy

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -51,7 +51,7 @@ const Contact = () => {
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="test-4xl text-accent">Let's work together</h3>
+              <h3 className="test-4xl text-accent">Let&apos;s work together</h3>
               <p className="text-white/60">
                 Fill out the form and describe your project needs. I&apos;ll get back
                 to you with a quote as soon as possible.
